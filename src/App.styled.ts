9d19fbcd3c6beb1ad788afc0e5d10eac174feb57
seokzin <seokzin95@gmail.com>
2022-10-05 15:43:45 +0900

    feat: 반응형 기본 CSS 적용

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
-export const Layout = styled.div``;
+export const Layout = styled.div`
+  @media ${(props) => props.theme.mobile} {
+    background-color: ${(props) => props.theme.subColor};
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { ThemeProvider } from 'styled-components';
+
 import { Button } from './components';
 import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
 
 import { Layout } from './App.styled';
 
 const App = () => {
   return (
-    <Layout>
-      <GlobalStyle />
-      Hello
-      <Button />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <GlobalStyle />
+        Hello
+        <Button />
+      </Layout>
+    </ThemeProvider>
   );
 };
 
