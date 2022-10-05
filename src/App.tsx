376abda6da012f7219b11a85b0443f5f00b48942
seokzin <seokzin95@gmail.com>
2022-10-05 16:18:34 +0900

    feat: react-query 설치 및 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-import { Button, Header } from './components';
+import { Button, Header, List } from './components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
 import { Layout } from './App.styled';
 
 const App = () => {
+  const client = new QueryClient();
+
   return (
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <GlobalStyle />
-        <Header />
-        Hello
-        <Button />
-      </Layout>
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ReactQueryDevtools />
+
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <GlobalStyle />
+          <Header />
+          Hello
+          <List />
+          <Button />
+        </Layout>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
