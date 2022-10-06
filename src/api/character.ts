5177670e36d35ac3adb8080ea5f282ae9e9aeebd
seokzin <seokzin95@gmail.com>
2022-10-06 16:07:57 +0900

    feat: 팩토리 함수로 API 응답 변환

--- src/api/character.ts
+++ src/api/character.ts
@@ -1,16 +1,33 @@
 import axios from 'axios';
 
+export interface Character {
+  name: string;
+  aliases: string[];
+  titles: string[];
+  books: string[];
+  tvSeries: string[];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const characterFactory = (raw: any): Character => ({
+  name: raw.name,
+  aliases: raw.aliases,
+  titles: raw.titles,
+  books: raw.books,
+  tvSeries: raw.tvSeries,
+});
+
 const api = axios.create({
   baseURL: 'https://www.anapioficeandfire.com/api/characters',
 });
 
-export const getCharacters = () =>
-  api.get('').then((response) => response.data);
+export const getCharacters = (): Promise<Character> =>
+  api.get('').then((response) => response.data.map(characterFactory));
 
 export const getCharacterById = (id: number) =>
-  api.get(`/${id}`).then((response) => response.data);
+  api.get(`/${id}`).then((res) => characterFactory(res.data));
 
 export const getCharacterPerPage = (page: number, pageSize = 10) =>
   api
     .get(`?page=${page}&pageSize=${pageSize}`)
-    .then((response) => response.data);
+    .then((response) => response.data.map(characterFactory));
