6710e74a71453a6a6c4cfc68699e95c12aaad327
seokzin <seokzin95@gmail.com>
2022-10-07 10:40:41 +0900

    feat: Button 프로퍼티 추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,18 @@
-const Button = () => {
-  return <div>Button</div>;
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  icon?: JSX.Element;
+}
+
+const Button = ({ label, onClick, icon }: ButtonProps) => {
+  return (
+    <div>
+      <button onClick={onClick}>
+        {icon}
+        {label}
+      </button>
+    </div>
+  );
 };
 
 export default Button;
