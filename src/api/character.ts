304282c525878f8db5e5ca5dd15de860be951264
seokzin <seokzin95@gmail.com>
2022-10-07 10:42:03 +0900

    feat: API reponse 프로퍼티 추가

--- src/api/character.ts
+++ src/api/character.ts
@@ -1,20 +1,28 @@
 import axios from 'axios';
 
 export interface Character {
+  id: string;
   name: string;
   aliases: string[];
   titles: string[];
   books: string[];
   tvSeries: string[];
+  gender: 'Male' | 'Female';
+  died: string;
 }
+
+const parseIdFromUrl = (url: string) => url.split('/').at(-1) as string;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const characterFactory = (raw: any): Character => ({
+  id: parseIdFromUrl(raw.url),
   name: raw.name,
   aliases: raw.aliases,
   titles: raw.titles,
   books: raw.books,
   tvSeries: raw.tvSeries,
+  gender: raw.gender,
+  died: raw.died,
 });
 
 const api = axios.create({
