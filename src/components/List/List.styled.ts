be7ae5b9a26e3320b419a92ea964bf8eeabddf97
seokzin <seokzin95@gmail.com>
2022-10-08 15:54:43 +0900

    feat: 페치된 데이터 flat 처리

---
+++ src/components/List/List.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 6rem;
+
+  font-size: 1.5rem;
+  color: #888888;
+`;
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,22 +3,30 @@ import { useInfiniteQuery } from 'react-query';
 import { getCharacterPerPage, PAGE_SIZE } from '../../api/character';
 import { Character } from '../../api/character';
 import Card from '../Card';
+import { Loading } from './List.styled';
 
 const List = () => {
-  const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage } =
-    useInfiniteQuery(
-      'characters',
-      ({ pageParam = 1 }) => getCharacterPerPage(pageParam),
-      {
-        getNextPageParam: (lastPage, pages) => {
-          if (lastPage.length < PAGE_SIZE) {
-            return undefined;
-          }
+  const { data, fetchNextPage, isFetching } = useInfiniteQuery(
+    'characters',
+    ({ pageParam = 1 }) => getCharacterPerPage(pageParam),
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if (lastPage.length < PAGE_SIZE) {
+          return undefined;
+        }
 
-          return pages.length + 1;
-        },
-      }
-    );
+        return pages.length + 1;
+      },
+    }
+  );
+
+  const flattenData = useCallback(() => {
+    if (!data) {
+      return [];
+    }
+
+    return data.pages.flat();
+  }, [data]);
 
   const handleScroll = useCallback(() => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
@@ -35,20 +43,13 @@ const List = () => {
   }, [handleScroll]);
 
   return (
-    <>
-      {data?.pages.map((page: Character[], index: number) => (
-        <div key={index}>
-          {page.map((character) => (
-            <Card key={character.id} {...character} />
-          ))}
-        </div>
+    <div>
+      {flattenData().map((character: Character) => (
+        <Card {...character} />
       ))}
-      <div>
-        <p>Is Loading: {isFetching ? 'Yes' : 'No'}</p>
-        <p>Is Loading Next Page: {isFetchingNextPage ? 'Yes' : 'No'}</p>
-        <p>Has Next Page: {hasNextPage ? 'Yes' : 'No'}</p>
-      </div>
-    </>
+
+      {isFetching && <Loading>Loading..</Loading>}
+    </div>
   );
 };
 
